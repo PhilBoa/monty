b8c5fab89f22b2bb175478c5d1f09b4ddf6f3cef
PhilBoa <philicbrefo98@.gmail.com>
2023-05-25 21:26:05 -0700

    pint

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include"monty.h"
+
+/**
+ * pint - print function
+ * Description: prints the element at the top of the stack
+ * @topptr: points to the top node in the stack
+ * @err_flag: a flag which it's value is 0 and will be updated
+ * if the stack is empty
+*/
+
+void pint(stack_t **topptr, int *err_flag)
+{
+	if (topptr == NULL)
+	{
+	*err_flag = 49;
+	return;
+	}
+	printf("%d\n", temp->n);
+}
